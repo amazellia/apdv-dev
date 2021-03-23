83a2c477be52f5de01941b06b1275aad0b7c35a4
Amanda Patricia Viray <amazellia@users.noreply.github.com>
2021-03-23 23:46:29 +1100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
-
+import '../styles/globals.scss'
+//import { Provider } from '@lyket/react';
+/*
+ReactDOM.render(
+  <Provider apiKey="c3fa0100ec0fc478a6c2233a06869e">
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+*/
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
