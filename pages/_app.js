82f96249bb69a6ec542646c558af1be999c3f1b7
Amanda Patricia Viray <amazellia@users.noreply.github.com>
2021-03-24 00:59:12 +1100

    commit on 'main'

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import '../styles/globals.scss'
-//import { Provider } from '@lyket/react';
-/*
-ReactDOM.render(
-  <Provider apiKey="c3fa0100ec0fc478a6c2233a06869e">
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-*/
+import ReactDOM from 'react-dom'
+import { Provider } from '@lyket/react'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+
+ReactDOM.render(
+  <Provider apiKey="c3fa0100ec0fc478a6c2233a06869e">
+    <MyApp />
+  </Provider>,
+  document.getElementById('root')
+)
